f2f8d67ec862fb71b5c667dfdaf048e7bd4e0a79
Madalina Erascu <madalina.erascu@e-uvt.ro>
2019-02-07 18:34:38 +0200

    no message

--- maneuverRecomadEngine/exposeRE.py
+++ maneuverRecomadEngine/exposeRE.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from problem.ProblemDefinition import ManeuverProblem
+from maneuverRecomadEngine.problem.ProblemDefinition import ManeuverProblem
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
